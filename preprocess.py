469be8a727c7995f3a7ca0ebfcbe8caa6d36b85d
jerica0527 <jerica0527@gmail.com>
2017-04-15 18:22:24 +0800

    add

--- preprocess.py
+++ preprocess.py
@@ -11,7 +11,7 @@ def transform(filename):
     """ your code here """
     
     df = filename
-    #print(df[:5])
+    #print(df[:15])
 
     df2 = np.array(df)
     #print(df.shape, df2.shape)
@@ -29,17 +29,39 @@ def transform(filename):
             else:
                 feature.append(x)   
         #print(df[df.columns[5]][:10])
-        df[df.columns[j]] = df[df.columns[j]].astype('category',ordered=True).values            
-        df[df.columns[j]] = df[df.columns[j]].cat.rename_categories(np.arange(len(feature)))
-        df[df.columns[j]].astype(int).values                        
-        #print(df[df.columns[5]][:10])
+        if df.columns[j] == 'Income':
+            #print(df[df.columns[j]][:10]) 
+            df[df.columns[j]] = df[df.columns[j]].astype('category', categories=[np.nan,"under $25,000",
+                "$25,001 - $50,000","$50,000 - $74,999","$75,000 - $100,000","$100,001 - $150,000",
+                "over $150,000"], ordered=False).values     
+            #print(df[df.columns[j]][50:70])      
+            df[df.columns[j]] = df[df.columns[j]].cat.rename_categories(np.insert(np.arange(len(feature),dtype=float),[0],[np.nan]))
+            df[df.columns[j]].astype(int).values
+            #print(df[df.columns[j]][50:70])   
+
+        elif df.columns[j] == 'EducationLevel':            
+            #print(df[df.columns[j]][:10]) 
+            df[df.columns[j]] = df[df.columns[j]].astype('category', categories=[np.nan,"Current K-12",
+                "High School Diploma","Current Undergraduate","Associate's Degree", 
+                "Bachelor's Degree","Master's Degree","Doctoral Degree"], ordered=False).values          
+            df[df.columns[j]] = df[df.columns[j]].cat.rename_categories(np.insert(np.arange(len(feature),dtype=float),[0],[np.nan]))
+            df[df.columns[j]].astype(int).values     
+            #print(df[df.columns[j]][:10]) 
+               
+        else:                       
+            df[df.columns[j]] = df[df.columns[j]].astype('category', ordered=True).values
+            df[df.columns[j]] = df[df.columns[j]].cat.add_categories([np.nan])            
+            #print(df[df.columns[j]].cat.categories.values)
+            #print(df[df.columns[j]][:15])
+            df[df.columns[j]] = df[df.columns[j]].cat.rename_categories(np.insert(np.arange(len(feature),dtype=float),[len(feature)],[np.nan]))
+            df[df.columns[j]].astype(int).values                        
+            #print(df[df.columns[j]][:15])
         
-    #print(df[:5])
+    #print(df[:15])
     
-    data = np.array(df)
-    target = df['Happy']  
-    
-    #print(df['Party'][19:100])
+    target = df['Happy']
+    data = df.drop('Happy',axis=1)
+       
     return {'data':data,'target':target}
 
 def fill_missing(X, strategy, isClassified):
@@ -54,9 +76,21 @@ def fill_missing(X, strategy, isClassified):
     """
     """ your code here """
     
-    
-    
-    return X_full
+    if (isClassified == 0):       
+        for j in range(3,4):#X.shape[1]):
+            name = X.columns[j]
+            Col_j = np.array(X[name])
+                       
+            #print(X[name][60:70]) 
+            mean = np.nanmean(Col_j,axis=0)
+            for i in range(len(Col_j)):
+                if (np.isnan(Col_j[i])):                
+                    #X[name][i] = mean
+                    break
+            #print(X[name][60:70])    
+            
+                  
+    #return X_full
     
 def main():
     ## Read the raw data with pandas.read_csv()
@@ -65,10 +99,15 @@ def main():
     #print(len(df.dtypes))
     
     Dict = transform(df)
+    X = Dict['data']
+    y = Dict['target']
     
-    print(Dict['data'].shape, Dict['target'].shape)
-    
-    #fill_missing(Dict['data'], strategy, isClassified)
+    print(X.shape, y.shape)
+
+    #cats = pd.Categorical([1,0], categories=[1,0])
+    #print(Dict['data']['Gender'].groupby(cats).mean())
+
+    fill_missing(X, 1, 0)
 
 if __name__ == '__main__':
     main()
